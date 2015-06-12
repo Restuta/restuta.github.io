abf3316a9ee330f8db862bbf3487c61828e2323c
Restuta <restuta8@gmail.com>
2015-06-11 23:17:03 -0700

    added small octopus =)

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,10 +1,17 @@
-console.log('This would be the main JS file.');
+console.log("" +
+    "          ,'\"\"`." + '\n' +
+    '         / _  _ \\' + '\n' +
+    "         |(@)(@)|" + '\n' +
+    "         )  __  (        hi there :D" + '\n' +
+    "        /,'))((`.\\" + '\n' +
+    "       (( ((  )) ))" + '\n' +
+    "        `\ `)(' /'");
 
- Mousetrap.bind(['command+shift+s', 'ctrl+shift+s'], function() {
+Mousetrap.bind(['command+shift+s', 'ctrl+shift+s'], function() {
 
-        $('.link-hidden').toggleClass('link-used-to-be-hidden');
+    $('.link-hidden').toggleClass('link-used-to-be-hidden');
 
-        // return false to prevent default browser behavior
-        // and stop event from bubbling
-        return false;
+    // return false to prevent default browser behavior
+    // and stop event from bubbling
+    return false;
 });
